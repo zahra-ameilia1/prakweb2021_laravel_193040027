ac345205726a300d1fffeff5ad125e2a0fdf75dc
itsmerawbby <60546046+zahra-ameilia1@users.noreply.github.com>
2021-10-28 04:25:30 +0700

    Menyelesaikan video 12

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -48,6 +49,7 @@ Route::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'title' => "Post By Category : $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load('author', 'category')
     ]);
 });
