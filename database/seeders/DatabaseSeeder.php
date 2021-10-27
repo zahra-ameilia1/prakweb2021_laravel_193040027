9b8bd4cce6eb686a2c3bf8c8321d01d385aa9c92
itsmerawbby <60546046+zahra-ameilia1@users.noreply.github.com>
2021-10-27 19:20:44 +0700

    Menyelesaikan video 9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Zahra Dwi Ameilia',
+            'email' => 'zahradwiameilia1@gmail.com',
+            'password' => bcrypt ('12345')
+        ]);
+
+        User::create([
+            'name' => 'Faira Mustika',
+            'email' => 'username12345@gmail.com',
+            'password' => bcrypt ('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, eaque excepturi officiis est commodi ratione necessitatibus quo quidem sint aut.',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem magnam unde odio vitae! Dolorem consectetur ipsam, quis voluptatem soluta cum eius, hic impedit culpa molestiae placeat repudiandae explicabo laborum. Maxime, sint! In vero qui, quod debitis ab accusantium cumque nam distinctio, accusamus facilis fugit est eaque velit quae aliquam necessitatibus nulla sunt delectus soluta dolor corporis possimus! Nesciunt est, quo temporibus quos molestias commodi repudiandae ullam reprehenderit! Nemo error voluptatibus quo eaque unde facilis optio sequi iusto cupiditate vero cumque fugit quisquam ipsam ea molestias, mollitia corrupti voluptate ipsa voluptates. Explicabo, quae labore error facere ea nostrum at voluptatum corporis.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, eaque excepturi officiis est commodi ratione necessitatibus quo quidem sint aut.',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem magnam unde odio vitae! Dolorem consectetur ipsam, quis voluptatem soluta cum eius, hic impedit culpa molestiae placeat repudiandae explicabo laborum. Maxime, sint! In vero qui, quod debitis ab accusantium cumque nam distinctio, accusamus facilis fugit est eaque velit quae aliquam necessitatibus nulla sunt delectus soluta dolor corporis possimus! Nesciunt est, quo temporibus quos molestias commodi repudiandae ullam reprehenderit! Nemo error voluptatibus quo eaque unde facilis optio sequi iusto cupiditate vero cumque fugit quisquam ipsam ea molestias, mollitia corrupti voluptate ipsa voluptates. Explicabo, quae labore error facere ea nostrum at voluptatum corporis.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, eaque excepturi officiis est commodi ratione necessitatibus quo quidem sint aut.',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem magnam unde odio vitae! Dolorem consectetur ipsam, quis voluptatem soluta cum eius, hic impedit culpa molestiae placeat repudiandae explicabo laborum. Maxime, sint! In vero qui, quod debitis ab accusantium cumque nam distinctio, accusamus facilis fugit est eaque velit quae aliquam necessitatibus nulla sunt delectus soluta dolor corporis possimus! Nesciunt est, quo temporibus quos molestias commodi repudiandae ullam reprehenderit! Nemo error voluptatibus quo eaque unde facilis optio sequi iusto cupiditate vero cumque fugit quisquam ipsam ea molestias, mollitia corrupti voluptate ipsa voluptates. Explicabo, quae labore error facere ea nostrum at voluptatum corporis.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, eaque excepturi officiis est commodi ratione necessitatibus quo quidem sint aut.',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem magnam unde odio vitae! Dolorem consectetur ipsam, quis voluptatem soluta cum eius, hic impedit culpa molestiae placeat repudiandae explicabo laborum. Maxime, sint! In vero qui, quod debitis ab accusantium cumque nam distinctio, accusamus facilis fugit est eaque velit quae aliquam necessitatibus nulla sunt delectus soluta dolor corporis possimus! Nesciunt est, quo temporibus quos molestias commodi repudiandae ullam reprehenderit! Nemo error voluptatibus quo eaque unde facilis optio sequi iusto cupiditate vero cumque fugit quisquam ipsam ea molestias, mollitia corrupti voluptate ipsa voluptates. Explicabo, quae labore error facere ea nostrum at voluptatum corporis.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,10 +3,10 @@
 @section('container')
   <article>
     <h2>{{ $post->title }}</h2>
-    <p>By. Zahra Dwi Ameilia in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/ {{ $post->category->slug }}" class="text-decoration-none">>{{ $post->category->name }}</a></p>
     {!! $post->body !!}
     
   </article>
 
-  <a href="/posts">Back to Posts</a>
+  <a href="/posts" class="d-block mt-3">Back to Posts</a>
 @endsection
