85cb3877174759ebc74869bb06cdedaf93c9d5fb
itsmerawbby <60546046+zahra-ameilia1@users.noreply.github.com>
2021-10-28 00:06:44 +0700

    Selesai video ke 10

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,8 @@
 @section('container')
   <article>
     <h2>{{ $post->title }}</h2>
-    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/ {{ $post->category->slug }}" class="text-decoration-none">>{{ $post->category->name }}</a></p>
+    <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+
     {!! $post->body !!}
     
   </article>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -48,5 +50,12 @@ Route::get('/categories/{category:slug}', function (Category $category) {
         'title' => $category->name,
         'posts' => $category->posts,
         'category' => $category->name
+    ]);
+});
+
+Route::get('/authors/{author:username}', function(User $author) {
+    return view('posts', [
+        'title' => 'User Posts',
+        'posts' => $author->posts,
     ]);
 });
