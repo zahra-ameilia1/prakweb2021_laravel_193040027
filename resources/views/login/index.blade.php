1dad6c51df1cbfc1a8e38f17cc773989469aee3b
itsmerawbby <60546046+zahra-ameilia1@users.noreply.github.com>
2021-11-19 15:55:40 +0700

    Menyelesaikan video 15 User Registration

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,11 +2,18 @@
 
 @section('container')
 <div class="row justify-content-center">
-    <div class="col-md-5">
+    <div class="col-md-4">
+
+      @if (session()->has('success'))
+      <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      @endif
+
         <main class="form-signin">
             <h1 class="h3 mb-3 fw-normal">Please Login</h1>
             <form>
-          
               <div class="form-floating">
                 <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
                 <label for="floatingInput">Email address</label>
