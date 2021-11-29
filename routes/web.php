5024864a3de0333eb3ce56be4a36e0c2f383b3ee
itsmerawbby <60546046+zahra-ameilia1@users.noreply.github.com>
2021-11-29 10:20:04 +0700

    Menyelesaikan video 17 Dashboard UI

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -74,4 +74,8 @@ Route::post('/register', [RegisterController::class, 'store']);
 //     ]);
 // });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function(){
+    return view('dashboard.index');
+})->middleware('auth');
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
