51ada470c2c85e7fc21d8db0856d379ee241d5ff
itsmerawbby <60546046+zahra-ameilia1@users.noreply.github.com>
2021-12-08 17:20:51 +0700

    Menyelesaikan Video 20 Update & Delete Post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -6,16 +6,19 @@
       <div class="col-lg-8">
         <h1 class="mb-3">{{ $post->title }}</h1>
   
-        <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span>Back to all My Posts</a>
-        <a href="" class="btn btn-warning"><span data-feather="edit"></span>Edit</a>
-        <a href="" class="btn btn-danger"><span data-feather="x-circle"></span>Delete</a>
+        <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all My Posts</a>
+        <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+        <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+          @method('delete')
+          @csrf
+          <button class="btn btn-danger" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span>  Delete</button>
+        </form>
   
         <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
   
         <article class="my-3 fs-5">
           {!! $post->body !!}
         </article>
-          
       </div>
     </div>
   </div>
